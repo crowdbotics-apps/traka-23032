aa58a811f1b8e69365bedba861558c7e58b054d7
Crowdbotics <team@crowdbotics.com>
2020-11-28 10:28:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/traka_23032/MainActivity.java
@@ -0,0 +1,15 @@
+package com.traka_23032;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "traka_23032";
+    }
+}
